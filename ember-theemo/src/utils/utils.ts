5585956097f94b3439ecf3027e3a16b2bc10975e
gossi <gossi@users.noreply.github.com>
2025-07-09 14:12:57 +0200

    Adjust code

--- ember-theemo/src/utils/utils.ts
+++ ember-theemo/src/utils/utils.ts
@@ -1,8 +1,20 @@
-import { getOwner } from '@ember/application';
+import { dependencySatisfies, importSync, macroCondition } from '@embroider/macros';
 import { assert } from '@ember/debug';
 
 import type { TheemoConfig } from '../types';
 import type Application from '@ember/application';
+
+import type { getOwner as getOwnerType } from '@ember/owner';
+
+interface OwnerModule {
+  getOwner: typeof getOwnerType;
+}
+
+const { getOwner } = (
+  macroCondition(dependencySatisfies('ember-source', '>=4.10'))
+    ? importSync('@ember/owner')
+    : importSync('@ember/application')
+) as OwnerModule;
 
 const SCRIPT_ID = 'theemo-config';
 
@@ -10,8 +22,10 @@ export function extractConfig(context: unknown): TheemoConfig {
   let script = document.getElementById(SCRIPT_ID);
 
   if (!script) {
-    // in case the app runs in shadow DOM, we need to query the script from the shadow root
-    const rootElement = (getOwner(context) as Application).rootElement;
+    // in case the app runs in shadow DOM, we need to query the script from the
+    // shadow root
+    // @ts-ignore
+    const rootElement = (getOwner(context) as unknown as Application).rootElement;
 
     if (rootElement instanceof HTMLElement) {
       script = (rootElement.getRootNode() as HTMLElement).querySelector(`[id="${SCRIPT_ID}"]`);
