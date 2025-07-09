c8fd238de9788ccbbff6102bf73141f9fb799f2b
gossi <gossi@users.noreply.github.com>
2025-07-09 14:13:07 +0200

    Restore tests

--- testing/test-app/tests/test-helper.ts
+++ testing/test-app/tests/test-helper.ts
@@ -1,13 +1,15 @@
 import { setApplication } from '@ember/test-helpers';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 
 import Application from 'test-app/app';
 import config from 'test-app/config/environment';
 
-setup(QUnit.assert);
-
 setApplication(Application.create(config.APP));
-
+setup(QUnit.assert);
+setupEmberOnerrorValidation();
+loadTests();
 start();
+
