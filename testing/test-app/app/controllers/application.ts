0182450371078acf1dd858e55beb656199795a27
gossi <gossi@users.noreply.github.com>
2025-07-10 18:20:47 +0200

    support ember pre 4.1

--- testing/test-app/app/controllers/application.ts
+++ testing/test-app/app/controllers/application.ts
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
-import { type Registry as Services, service } from '@ember/service';
+import * as serviceMod from '@ember/service';
+
+import type { TheemoService } from 'ember-theemo';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+const service = serviceMod.service ?? serviceMod.inject;
 
 export default class ApplicationController extends Controller {
-  @service declare theemo: Services['theemo'];
+  @service declare theemo: TheemoService;
 }
