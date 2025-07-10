90855962c418887e2a7b8a4de60988d73aaa8a24
gossi <gossi@users.noreply.github.com>
2025-07-10 19:24:28 +0200

    fix lint

--- testing/test-app/app/routes/application.ts
+++ testing/test-app/app/routes/application.ts
@@ -3,7 +3,7 @@ import * as serviceMod from '@ember/service';
 
 import type { TheemoService } from 'ember-theemo';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, @typescript-eslint/no-deprecated
 const service = serviceMod.service ?? serviceMod.inject;
 
 export default class ApplicationRoute extends Route {
